813cdbb0fbc2171e1c3f56b3b98881f22ab2aa3b
DennisKipkirui <kipkiruimutai33@gmail.com>
2020-10-28 09:32:03 +0300

    fix bugs

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app,db
 from flask_script import Manager, Server
-from app.models import User, Pitch, Comments, PitchCategory
+from app.models import User, Pitch, Comment
 
 from flask_migrate import Migrate, MigrateCommand
 
@@ -26,7 +26,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Pitch = Pitch, Comments=Comments, PitchCategory=PitchCategory)
+    return dict(app=app, db=db, User=User, Pitch = Pitch, CommentS=Comment)
     # pass
 
 
